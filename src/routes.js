9a17fcf221fec6013430d6de3d1e763cac7a2c7d
Erackson souza <souza.erackson@gmail.com>
2025-11-15 16:03:36 -0300

    feat: implement database functionality and routes GET for  lead search

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,34 @@
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+
+const database = new Database();
+
 export const routes = [
+  {
+    method: "GET",
+    path: "/leads",
+    handler: (req, res) => {
+      const leads = database.select("leads");
+      return res.writeHead(200).end(JSON.stringify(leads));
+    },
+  },
   {
     method: "POST",
     path: "/leads",
     handler: (req, res) => {
       const { name, email } = req.body;
 
-      const users = {
-        id: 1,
+      const lead = {
+        id: randomUUID(),
         name,
         email,
       };
 
+      database.insert("leads", lead);
+
       return res
         .writeHead(201)
-        .end(JSON.stringify("Lead created successfully"));
+        .end(JSON.stringify({ message: "Lead criado com sucesso!", lead }));
     },
   },
 ];
